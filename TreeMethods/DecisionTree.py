8aa3da6530ff2b199157e78d361779a124d3fd4b
Mike <mdh266@gmail.com>
2017-02-03 15:58:08 -0500

    fixed indentation

--- TreeMethods/DecisionTree.py
+++ TreeMethods/DecisionTree.py
@@ -65,27 +65,6 @@ class DecisionTree:
 
 		**root** (:class:`TreeNode`): 
 			The root of the decision tree.
-
-	:Example:
-		.. code:: python
-	
-		>>> dataset = [[2.771244718, 1.784783929, 0],
-			       [1.728571309, 1.169761413, 0],
-			       [3.678319846, 2.81281357, 1],
-			       [3.961043357, 2.61995032, 1],
-			       [2.999208922, 2.209014212, 0],
-			       [7.497545867, 3.162953546, 0],
-			       [9.00220326, 3.339047188, 1],
-			       [7.444542326, 0.476683375, 1],
-			       [10.12493903, 3.234550982, 0],
-			       [6.642287351, 3.319983761, 1]]
-		>>> 
-		>>> df = pd.DataFrame(data=dataset,columns =['feature_1','feature_2','target'])
-		>>> tree = DecisionTree(2,1)
-		>>> tree.fit(df,target='target')
-		>>> data_point = pd.Series([2.0, 23.0], index=['feature_1','feature_2'])
-		>>> tree.predict(data_point)
-		0
 	"""
 
 	def __init__(self, max_depth=2, min_size=5):
@@ -141,16 +120,16 @@ class DecisionTree:
 
 	def _test_split(self, feature, value, dataset):
 		"""
-        Split a dataset based on an attribute and an attribute value
-    
-        :param str feature: The feature column name to do the splitting with respect to.
-        :param float value: The splitting value
-        :parm DataFrame dataset: The Pandas DataFrame dataset.
-    
-        :return: Returns a list of the split dataset [left dataframe, right dataframe]
+		Split a dataset based on an attribute and an attribute value
+
+		:param str feature: The feature column name to do the splitting with respect to.
+		:param float value: The splitting value
+		:parm DataFrame dataset: The Pandas DataFrame dataset.
+
+		:return: Returns a list of the split dataset [left dataframe, right dataframe]
        	
 		:rtype: List of Pandas Dataframes
-        """
+		"""
 		# Left data frame which has all data points with the specific feature
 		# values less than value.
 		left = dataset[dataset[feature] < value]
@@ -199,11 +178,11 @@ class DecisionTree:
 
 	def _print_tree(self, node, depth):
 		"""
-        Inner recursive call for printing the tree.
+		Inner recursive call for printing the tree.
 		
 		:param TreeNode node: The TreeNode node in the binary tree.
 		:oaram int depth: The depth of this node in the tree.
-        """
+		"""
 		if isinstance(node.val, dict):
 			print('%s [%s < %.3f]' % ((depth*' ',
 				 node.val['splitting_feature'],
