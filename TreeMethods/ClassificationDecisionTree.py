59d9eacfacc992bd11666bcd19ca16d955370a17
Mike <mdh266@gmail.com>
2017-02-03 15:43:49 -0500

    Initial commit.

---
+++ TreeMethods/ClassificationDecisionTree.py
@@ -0,0 +1,232 @@
+from random import randrange  
+import pandas as pd
+
+from DecisionTree import DecisionTree
+from DecisionTree import TreeNode
+
+class DecisionTreeClassifier (DecisionTree):
+	
+	def __init__(self, 
+                 max_depth=2,
+                 min_size=5):
+		"""
+        Constructor for a classification decision tree.
+
+     	:param int max_depth: The maximum depth of tree.
+        :param int min_size: The minimum number of datapoints in terminal nodes.
+        """
+		DecisionTree.__init__(self,
+							  max_depth,
+							  min_size)
+	
+	def fit(self,
+			dataset,
+			target):
+		"""
+		Builds the decsision tree by recursively splitting tree until the
+		the maxmimum depth of the tree is acheived or the nodes have the
+		min_size number of training points.
+		
+		:parameters:
+			**dataset** (`DataFrame <http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html>`_ ):
+				Training data.
+			
+			**target** (str): 
+				The column name of the target in the dataset.
+		"""
+		self.original_n_features = dataset.shape[1] -1
+
+		# If the number of features used to choose the split hasnt been specified
+		# then set it the number of features in the dataset.
+		if self.n_features == None:
+			self.n_features = self.original_n_features
+
+		# Get the first split of the dataset
+		node_value = self._get_split(dataset, 
+							  target)
+
+		# Creates the root with val node_value
+		self.root = TreeNode(node_value)
+
+		# Now recursively split the tree
+		self._split(self.root,
+					dataset,
+					target,
+					1)
+
+
+	def _get_split(self, 
+                   dataset, 
+				   target):
+		"""
+        Select the best split point for a dataset using a random
+        selection of the features.
+    
+        :parameters:
+			 dataset:Training data set, needs to be Pandas DataFrame.
+		:param str target: The column name of the target.
+		
+        :return: Returns a 
+        	dictionary: {
+						'splitting_feature': (str) the feature column name 
+											  which the split is chosen on.
+
+						'splitting_value'   :  (float) the value used to split
+											  the data set according the feature.
+						}
+		:rtype: dict
+        """
+		best_feature, best_value, best_score, best_groups = 999,999,999,None
+
+		# the features to test among the split
+		features = list()
+		cols = dataset.columns
+
+		# randomily select features to consider 
+		while len(features) < self.n_features:
+
+			# out of all the features
+			feature_col = randrange(dataset.shape[1]-1)
+ 
+			# exclude the target as a possible feature col
+			if cols[feature_col] not in features and cols[feature_col] != target:
+				features.append(cols[feature_col])
+		
+		# loop through the number of features to figure out which
+		# gives the best split.
+		for feature in features:
+			# split the data set according to this feature
+			# and find the splits gini_index
+			feature_values = dataset[feature]
+			for index_val_pair in feature_values.iteritems():
+
+				left_group, right_group = self._test_split(feature, 
+										  		  		   index_val_pair[1], 
+										  		  		   dataset)
+
+				gini = self._gini_index(left_group[target],
+										right_group[target])
+			
+				# if this is the best split update the info
+				if gini < best_score:
+					best_feature = feature
+					best_value = index_val_pair[1]
+					best_score = gini
+					#best_groups = [left_group, right_group]
+
+		return {'splitting_feature': best_feature,
+                'splitting_value': best_value}
+
+
+
+	
+
+
+	def _gini_index(self, 
+					target_group_1,
+					target_group_2):
+		"""
+		Calculates the Gini index for a split dataset by counting up
+		the percentages of each targets classes in the split groups.
+	
+		:param Series target_group_1: The Panda Series of target values in
+			 group 1 after splitting.
+		:param Series target_group_1: The Panda Series of target values in
+			 group 2 after splitting.
+		
+		:return: Returns the gini-index
+		:rtype: float
+        """
+
+		gini = 0.0
+		
+		if target_group_1.empty is False:
+			# get the percentage of class types in the first group
+			group_1_percentages = target_group_1.value_counts(normalize=True)
+
+			# loop over and get the percentages of the class in this group
+			for class_percent_tupple in group_1_percentages.iteritems():
+				percentage = class_percent_tupple[1]
+				
+				#update gini index
+				gini += percentage * (1 -percentage)
+		
+		if target_group_2.empty is False:
+		# get the percentage of class types in the first group
+			group_2_percentages = target_group_2.value_counts(normalize=True) 
+			
+			# loop over and get the percentages of the class in this gruop
+			for class_percent_tupple in group_2_percentages.iteritems():
+				percentage = class_percent_tupple[1]
+
+				#update gini index
+				gini += percentage * (1 -percentage)
+	
+		return gini
+
+
+	def _split(self, 
+               curr, 
+			   dataset,
+			   target,
+               depth):
+		"""
+        Recursive splitting function that creates child
+        splits for a node or make this node a terminal node.
+        """
+		left_df = dataset[dataset[curr.val['splitting_feature']] < curr.val['splitting_value']]
+		right_df = dataset[dataset[curr.val['splitting_feature']] >= curr.val['splitting_value']]
+
+		# check if either split dataset is empty
+		if left_df.empty or right_df.empty:
+			curr.val = self._make_leaf(dataset[target])
+
+		# deal with tree being at max_depth
+		elif depth >= self.max_depth:
+			del(dataset)
+			curr.left = TreeNode(self._make_leaf(left_df[target]))
+			curr.right = TreeNode(self._make_leaf(right_df[target]))
+		else:
+			del(dataset)
+			if left_df.shape[0] <= self.min_size:
+				curr.left = TreeNode(self._make_leaf(left_df[target]))
+			else:
+				curr.left = TreeNode(self._get_split(left_df,
+													target))
+
+				self._split(curr.left,
+						left_df,
+						target,
+						depth+1)
+
+			# process left child
+			if right_df.shape[0] <= self.min_size:
+				curr.right = TreeNode(self._make_leaf(right_df[target]))
+			else:
+				curr.right = TreeNode(self._get_split(right_df,
+										target))
+
+				self._split(curr.right,
+							right_df,
+							target,
+							depth+1)
+		return
+
+	def _make_leaf(self,
+                target_values):
+		"""
+        Creates a terminal node value by selecting amoung the group that has
+        the majority.
+    
+        Input:
+        @node - of the decision tree
+    	@targer_values
+        """
+		# not sure i need this check
+		if len(target_values.unique()) == 1:
+			value = target_values.unique()[0]
+		else:
+			value = target_values.value_counts(normalize=True).argmax()
+		
+
+		return value
---
+++ TreeMethods/DecisionTree.py
@@ -0,0 +1,224 @@
+from random import randrange  
+import pandas as pd
+
+
+class TreeNode:
+	"""
+	TreeNode is a class used to build a binary decison tree.
+
+	:Parameters:
+		**value** (dict or int): 
+			If the node is a leaf, value is the predicted class otherwise it's a dictionary.
+		
+
+	:Attributes: 
+
+		**val** (dict or int): 
+			If the node is a leaf, val is the predicted class otherwise it's a dictionary.
+	
+	 	**left** (:class:`TreeNode`): 
+			The left child.
+
+		**right** (:class:`TreeNode`): 
+			The right child.
+		
+	"""
+	def __init__(self, value):
+		"""
+		Constructor for TreeNode in the decision tree. 
+	
+		:param dict or int value: If the node is a leaf, value is the predicted class otherwise it's a dictionary.
+		"""
+		# dictionary if not terminal node, otherwise it it the class value.
+		self.val = value
+		
+		# left child
+		self.left = None
+
+		# right child
+		self.right = None
+
+
+class DecisionTree:
+	"""
+	A decision tree classifier based off the gini-index.
+	
+	:Parameters:
+		**max_depth** (int):
+			The maximum depth of tree.
+
+		**min_size** (int):
+			The minimum number of datapoints in terminal nodes.
+		
+	:Attributes: 
+		**max_depth** (int): 
+			The maximum depth of tree.
+
+		**min_size** (int):
+			The minimum number of datapoints in terminal nodes.
+
+		**original_n_features** (int): 
+			The number of features in the dataset.
+
+		**n_features** (int): 
+			The number of randomly chosen features to be used in splitting.
+
+		**root** (:class:`TreeNode`): 
+			The root of the decision tree.
+
+	:Example:
+		.. code:: python
+	
+		>>> dataset = [[2.771244718, 1.784783929, 0],
+			       [1.728571309, 1.169761413, 0],
+			       [3.678319846, 2.81281357, 1],
+			       [3.961043357, 2.61995032, 1],
+			       [2.999208922, 2.209014212, 0],
+			       [7.497545867, 3.162953546, 0],
+			       [9.00220326, 3.339047188, 1],
+			       [7.444542326, 0.476683375, 1],
+			       [10.12493903, 3.234550982, 0],
+			       [6.642287351, 3.319983761, 1]]
+		>>> 
+		>>> df = pd.DataFrame(data=dataset,columns =['feature_1','feature_2','target'])
+		>>> tree = DecisionTree(2,1)
+		>>> tree.fit(df,target='target')
+		>>> data_point = pd.Series([2.0, 23.0], index=['feature_1','feature_2'])
+		>>> tree.predict(data_point)
+		0
+	"""
+
+	def __init__(self, 
+                 max_depth=2,
+                 min_size=5):
+		"""
+        Constructor for a classification decision tree.
+
+     	:param int max_depth: The maximum depth of tree.
+        :param int min_size: The minimum number of datapoints in terminal nodes.
+        """
+		self.max_depth = max_depth
+		self.min_size = min_size
+		self.original_n_features = None
+		self.n_features =  None 
+		self.root = None
+
+
+	def set_n_features(self, n_features):
+		"""
+		Set the number of features to choose the split. This could be different
+		then the number of features in the data set because you one might
+		be using this class from a Random Forest.
+
+		
+		:parameters:
+			**n_features** (int): 
+				The number of features to choose the split.
+		"""
+		self.n_features = n_features
+
+
+	def predict(self,
+				row):
+		"""
+		Predict the class that this datapoint belongs to.
+
+		:parameters:
+			 **row** (`Series <http://pandas.pydata.org/pandas-docs/stable/generated/pandas.Series.html>`_): 
+				The datapoint to classify.
+		"""
+		
+		# check to make sure data point is the right size
+		assert row.shape[0] == self.original_n_features
+		
+		# Go down the tree until get a leaf.
+		curr = self.root
+		while isinstance(curr.val, dict):
+			if row[curr.val['splitting_feature']] < curr.val['splitting_value']:
+				curr = curr.left
+			else:
+				curr = curr.right
+
+		# now at a terminal node and get the predtion
+		return curr.val
+
+	def _test_split(self, 
+                    feature, 
+                    value, 
+                    dataset):
+		"""
+        Split a dataset based on an attribute and an attribute value
+    
+        :param str feature: The feature column name to do the splitting with respect to.
+        :param float value: The splitting value
+        :parm DataFrame dataset: The Pandas DataFrame dataset.
+    
+        :return: Returns a list of the split dataset [left dataframe, right dataframe]
+       	
+		:rtype: List of Pandas Dataframes
+        """
+		# Left data frame which has all data points with the specific feature
+		# values less than value.
+		left = dataset[dataset[feature] < value]
+		# Right data frame which has all data points with the specific feature
+		# values greater than equal to value.
+		right = dataset[dataset[feature] >= value]
+	
+		return [left,right]
+
+	def _to_string(self):
+		"""
+		Prints breadth first traversal of the tree to a string.
+
+		This is used for comparison in testing.
+
+		:return: Returns a string of the breadth first traversal.
+		:rtype: str
+		"""
+		s = ""
+		queue = [self.root]
+		visited = []
+		while len(queue) != 0:
+			curr = queue.pop(0)
+			if curr not in visited:
+				visited.append(curr)
+				if curr.left != None:
+					queue.append(curr.left)
+				if curr.right != None:
+					queue.append(curr.right)
+		
+		for node in visited:
+			if isinstance(node.val, dict):
+				s += '[ ' + node.val['splitting_feature'] + ' < '\
+					+  str(node.val['splitting_value']) + ' ] \n'
+			else:
+				s += str(node.val) + '\n'
+
+		return s
+
+	def print_tree(self):
+		"""
+		Prints the tree using a pre-order traversal.
+		"""
+		self._print_tree(self.root,1)
+
+
+	def _print_tree(self, 
+                    node, 
+                    depth):
+		"""
+        Inner recursive call for printing the tree.
+		
+		:param TreeNode node: The TreeNode node in the binary tree.
+		:oaram int depth: The depth of this node in the tree.
+        """
+		if isinstance(node.val, dict):
+			print('%s [%s < %.3f]' % ((depth*' ',
+				 node.val['splitting_feature'],
+				  node.val['splitting_value'])))
+
+			self._print_tree(node.left, depth+1)
+			self._print_tree(node.right, depth+1)
+		else:
+			print('%s[%s]' % ((depth*' ', node.val)))
+            
