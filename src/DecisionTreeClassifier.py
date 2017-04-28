c16b73083419df523f6f53bd80e5ae45648ffc9c
Mike <mdh266@gmail.com>
2017-04-28 12:28:15 -0400

    documented.

--- src/DecisionTreeClassifier.py
+++ src/DecisionTreeClassifier.py
@@ -1,38 +1,128 @@
 
 from DecisionTree import DecisionTree
+import math
 
 class DecisionTreeClassifier (DecisionTree):
+	"""
+	A decision tree classifier that extends the DecisionTree class. 
+
+	Attributes: 
+		max_depth (int): The maximum depth of tree.
+
+		min_size (int): The minimum number of datapoints in terminal nodes.
+
+		n_features (int): The number of features to be used in splitting.
+
+		root (dictionary): The root of the decision tree.
+
+		columns (list) : The feature names.
+
+		class_values (list) : The list of the target class values.
+
+		cost_function (str) : The name of the cost function to use: 'gini' or 'entropy'
+	"""
 
 	def __init__(self, max_depth=2, min_size=2, cost='gini'):
+		"""
+		Constructor for the Decision Tree Classifer.  It calls the base
+		class constructor and sets the cost function.  If the cost
+		parameter is not 'gini' or 'entropy' then an exception is thrown.
+
+		Args: 
+			max_depth (int): The maximum depth of tree.
+
+			min_size (int): The minimum number of datapoints in terminal nodes.
+
+			cost (str) : The name of the cost function to use: 'gini' or 'entropy'
+		"""
 		DecisionTree.__init__(self, max_depth, min_size)
 		self.class_values = None
-		self.cost_function = cost
+		self.cost_function = None
+		if cost == 'gini':
+			self.cost_function = cost
+		else:
+			raise NameError('Not valid cost function')
 
+		
 
-	def fit(self, train, n_features):
+	def fit(self, train, n_features=None):
+		"""
+		Builds the classification decsision tree by recursively splitting 
+		tree until the the maxmimum depth, max_depth of the tree is acheived or
+		the node have the minimum number of training points, min_size.
+		
+		Note: n_features will be passed by the RandomForest as it is 
+			  usually ta subset of the total number of features. 
+			  However, if one is using the class as a stand alone decision
+			  tree, then the n_features will automatically be 
+		
+		Args:
+			dataset (list) : list representation of the dataset.
+
+			n_features (int) : The number of features.
+		"""
 		self.class_values = list(set(row[-1] for row in train))
 		self._fit(train, n_features)
 
 	def predict(self, row):
+		"""
+		Predict the class that this sample datapoint belongs to.
+
+		Args:
+			row (list) : The datapoint to classify.
+
+		Returns:
+			int. The class the data points belong to.
+		"""
 		return self._predict(row, self.root)
 
 
 	def _cost(self, groups):
-		return self._gini_index(groups, self.class_values)
+		"""
+		Returns the associated cost for the split of the dataset 
+		into two groups. The cost_function will be set when the
+		tree is initialized.
 
+		Args: 
+			groups (list) : List of the two subdatasets after splitting.
 
-	def _gini_index(self, groups, class_values):
+		Returns:
+			float. Either the gini-index or entropy of the split.
+
+		"""
+		return self._gini_index(groups)
+
+	def _gini_index(self, groups):
+		"""
+		Returns the gini-index for the split of the dataset into two groups.
+
+		Args:
+			groups (list) : List of the two subdatasets after splitting.
+
+		Returns:
+			float. gini-index of the split.
+		"""
 		gini = 0.0
-		for class_value in class_values:
+		for class_value in self.class_values:
 			for group in groups:
 				size = len(group)
 				if size == 0:
 					continue
 				p = [row[-1] for row in group].count(class_value) / float(size)
 				gini += (p * (1.0 - p))
-				return gini
-				
+		return gini
+
 
 	def _make_leaf(self, group):
+		"""
+        Creates a terminal node value by selecting amoung the group that has
+        the majority.
+
+        Args: 
+        	group (list): The subgroup of the dataset.
+
+       	Returns:
+       		int. The majority of this groups target class values.
+    	"""
 		outcomes = [row[-1] for row in group]
 		return max(set(outcomes), key=outcomes.count)
--- src/RandomForestClassifier.py
+++ src/RandomForestClassifier.py
@@ -2,13 +2,71 @@ from math import sqrt
 from RandomForest import RandomForest
 from DecisionTreeClassifier import DecisionTreeClassifier
 
+
+
 class RandomForestClassifier (RandomForest):
 
+	"""
+	A random forest classifier that derives from the base class RandomForest.
+		
+	Attributes: 
+		n_trees (int) : The number of trees to use.
+
+		max_depth (int): The maximum depth of tree.
+
+		min_size (int): The minimum number of datapoints in terminal nodes.
+
+		cost_function (str) : The name of the cost function to use: 'gini'.
+
+		trees (list) : A list of the DecisionTree objects.
+
+		columns (list) : The feature names.
+	"""
+
 	def __init__(self, n_trees=10, max_depth=2, min_size=2, cost='gini'):
-		RandomForest.__init__(self, cost,  n_trees=10, max_depth=2, min_size=2)
+		"""
+		Constructor for random forest classifier. This mainly just initializez
+		the attributes of the class by calling the base class constructor. 
+		However, here is where it is the cost function string is checked
+		to make sure it either using 'gini', otherwise an error is thrown.
+
+		Args:
+			cost (str) : The name of the cost function to use for evaluating
+						 the split.
+
+			n_trees (int): The number of trees to use.
+
+			max_depth (int): The maximum depth of tree.
+
+			min_size (int): The minimum number of datapoints in terminal nodes.
+	
+		"""
+		if cost != 'gini':
+			raise NameError('Not valid cost function')
+		else:
+			RandomForest.__init__(self, cost,  n_trees=10, max_depth=2, min_size=2)
 		
 
-	def fit(self, train, test):
+	def fit(self, train, test=None):
+		"""
+		Fit the random forest to the training set train.  If a test set is provided
+		then the return value wil be the predictions of the RandomForest on the
+		test set.  If no test set is provide nothing is returned.
+
+
+		Note: Below we set the number of features to use in the splitting to be
+		the square root of the number of total features in the dataset.
+
+		Args:
+			train (list) : The training set.
+			test (list) : The test set.
+
+		Returns:
+			list or None. If a test set is provided then the return value wil be
+			the predictions of the RandomForest on the test set.  If no test set 
+			is provide nothing is returned.
+		"""
+		# set the number of features for the trees to use.
 		n_features = int(sqrt(len(train[0])-1))
 
 		for i in range(self.n_trees):
@@ -20,16 +78,41 @@ class RandomForestClassifier (RandomForest):
 			tree.fit(sample, n_features)
 			self.trees.append(tree)
 
-		predictions = [self.predict(row) for row in test]
-		return(predictions)
+		# if the test set is not empty then return the predictions
+		if test is not None:
+			predictions = [self.predict(row) for row in test]
+			return(predictions)
 
 
 	def predict(self, row):
+		"""
+		Peform a prediction for a sample data point by bagging 
+		the prediction of the trees in the ensemble. The majority
+		target class that is chosen.
+
+		Args: 
+			row (list ) : The data point to classify.
+
+		Returns: 
+			int.  The predicted target class for this data point.
+		"""
 		predictions = [tree.predict(row) for tree in self.trees]
 		return max(set(predictions), key=predictions.count)
 
 
 	def KFoldCV(self, dataset, n_folds=10):
+		"""
+		Perform k-fold cross validatation on the dataset 
+		and return the acrruracy of each training.
+
+		Args:
+			dataset (list) : The dataset in list form.
+			n_fold (int) : The number of folds in the k-fold CV.
+
+		Returns:
+			list. List of the accuracy of each Random Forest on each
+			of the folds.
+		"""
 		folds = self._cross_validation_split(dataset, n_folds)
 		scores = list()
 		for fold in folds:
@@ -49,9 +132,31 @@ class RandomForestClassifier (RandomForest):
 
 
 	def _metric(self, actual, predicted):
+		"""
+		Returns the accuracy of the predictions for now, extending it 
+		to include other metrics.
+
+		Args: 
+			actual (list) : The actual target values.
+			predicted (list) : The predicted target values.
+
+		Returns:
+			float.  The accuracy of the predictions.
+
+		"""
 		return self._accuracy(actual, predicted)
 
 	def _accuracy(self, actual, predicted):
+		"""
+		Computes the accuracy by counting how many predictions were correct.
+
+		Args: 
+			actual (list) : The actual target values.
+			predicted (list) : The predicted target values.
+
+		Returns:
+			float.  The accuracy of the predictions.
+		"""
 		correct = 0
 		for i in range(len(actual)):
 			if actual[i] == predicted[i]:
