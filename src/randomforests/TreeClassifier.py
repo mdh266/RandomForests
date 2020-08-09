fixing cost of split by using weighted average of each subsequent nodes cost, this was an error before

src/randomforests/TreeClassifier.py
@@ -101,10 +101,13 @@ class DecisionTreeClassifier (BaseEstimator, ClassifierMixin, DecisionTree ):
 
         """
         cost = 0.0
+        size = len(groups[0]) + len(groups[1])
         for group in groups:
-            cost += self._gini_index(group[:,-1])
+            split_size = len(group)
+            if split_size != 0:
+                cost += split_size * self._gini_index(group[:,-1])
 
-        return cost
+        return cost /size
 
 
     def _gini_index(self, y : np.ndarray) -> float:
@@ -117,10 +120,9 @@ class DecisionTreeClassifier (BaseEstimator, ClassifierMixin, DecisionTree ):
         target_val_cts = dict(zip(*np.unique(y_t, return_counts=True)))
         size = len(y)
 
-        if size != 0:
-            for target_class in target_val_cts:
-                p = target_val_cts[target_class] / size
-                gini += p * (1 - p)
+        for target_class in target_val_cts:
+            p = target_val_cts[target_class] / size
+            gini += p * (1 - p)
 
         return gini
 

src/randomforests/TreeRegressor.py
@@ -96,12 +96,14 @@ class DecisionTreeRegressor (BaseEstimator, ClassifierMixin, DecisionTree ):
 
         """
         cost = 0.0
+        size = len(groups[0]) + len(groups[1])
         for group in groups:
-            if len(group) > 0:
-                cost += np.std(group[:,-1]) ** 2
+            split_size = len(group)
+            if split_size > 0:
+                cost += split_size * np.var(group[:,-1])
             else:
                 continue
-        return cost
+        return cost / size
 
     def _make_leaf(self, y : np.ndarray) -> float :
         """

tests/integration/test_int_forest_reg.py
@@ -65,6 +65,6 @@ def test_predict(y):
     correct_size  = len(preds) == 4
 
     bounded_max   = np.max(preds) <= 0.9
-    bounded_in    = np.min(preds) >= 0.1
+    bounded_min   = np.min(preds) >= 0.1
 
-    assert correct_size and bounded_in and bounded_max
+    assert correct_size and bounded_min and bounded_max

tests/unit/test_unit_tree_clf.py
@@ -33,10 +33,10 @@ def test_gini_index(y, expected):
 
 gini_cost_tests =[ ((np.array([[0.],[0.], [0.],[0.]]),
                      np.array([[0.],[0.], [1.],[1.]])),
-                     0.5),
+                     0.25),
                    ((np.array([[0.],[1.], [0.],[0.]]),
                     np.array([[0.],[0.], [0.],[1.]])),
-                     0.75)
+                     0.375)
 ]
 
 @pytest.mark.parametrize('groups, expected', gini_cost_tests)

tests/unit/test_unit_tree_reg.py
@@ -38,10 +38,10 @@ def test_default_getparams():
 
 mse_cost_tests =[ ((np.array([[0.],[0.], [0.],[0.]]),
                     np.array([[0.],[0.], [1.],[1.]])),
-                   0.25),
+                   0.125),
                    ((np.array([[0.],[1.], [0.],[0.]]),
                     np.array([[0.],[0.], [0.],[1.]])),
-                   0.374999999)
+                   0.1874999995)
                   ]
 
 @pytest.mark.parametrize('groups, expected', mse_cost_tests)